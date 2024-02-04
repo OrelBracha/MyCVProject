a5494a9e9685c2235dab0150c0a02eb953363ac3
OrelBracha <orelbracha@yahoo.com>
2024-02-04 13:11:08 +0200

    Initial commit

---
+++ src/app/components/body/education/education.component.ts
@@ -0,0 +1,11 @@
+// education.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent {
+}
+
